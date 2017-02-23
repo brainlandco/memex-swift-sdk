3af996b9f9f838488194c9300bca77a3ef48708f
Adam Zdara <zdaraa@gmail.com>
2017-02-23 22:03:32 +0100

    Init

---
+++ MemexSDK/Build/Intermediates/MemexSDK.build/Debug-iphonesimulator/MemexSDK.build/Objects-normal/x86_64/MemexSDK_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/adamzdara/Memex/memex-swift-sdk/MemexSDK/Build/Intermediates/MemexSDK.build/Debug-iphonesimulator/MemexSDK.build/DerivedSources/MemexSDK_vers.c
---
+++ MemexSDK/Build/Intermediates/MemexSDK.build/Debug-iphonesimulator/MemexSDKTests.build/DerivedSources/MemexSDKTests_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char MemexSDKTestsVersionString[];
+ extern const double MemexSDKTestsVersionNumber;
+
+ const unsigned char MemexSDKTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:MemexSDKTests  PROJECT:MemexSDK-1" "\n";
+ const double MemexSDKTestsVersionNumber __attribute__ ((used)) = (double)1.;
