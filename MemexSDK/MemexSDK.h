fb8c01fab2f366acb2367a040526c205d00a95e4
Adam Zdara <zdaraa@gmail.com>
2017-03-31 18:34:15 +0200

    Review framework structure

---
+++ MemexSDK/MemexSDK.h
@@ -0,0 +1,19 @@
+//
+//  MemexSDK.h
+//  MemexSDK
+//
+//  Created by Adam Zdara on 31/03/2017.
+//  Copyright © 2017 Adam Zdara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MemexSDK.
+FOUNDATION_EXPORT double MemexSDKVersionNumber;
+
+//! Project version string for MemexSDK.
+FOUNDATION_EXPORT const unsigned char MemexSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MemexSDK/PublicHeader.h>
+
+
