7af631e55ef0bc03144f18b7d515f6e515e917d8
Adam Zdara <zdaraa@gmail.com>
2017-06-26 21:00:39 +0200

    Add links tests

---
+++ Example/Tests/LinksTests.swift
@@ -0,0 +1,111 @@
+import UIKit
+import XCTest
+import MemexSwiftSDK
+
+class LinksTests: BaseTestCase {
+  
+  func testCreation() {
+    let expectation1 = expectation(description: "default")
+    self.prepareSDK(authorize: true) { (memex, myself) in
+      let space = Space()
+      space.MUID = UUID().uuidString
+      memex.createSpace(space: space, process: .no, autodump: false, completion: { (newSpace, error) in
+        XCTAssertNil(error, "request failed")
+        
+        let link = Link()
+        link.originSpaceMUID = newSpace?.MUID
+        link.targetSpaceMUID = newSpace?.MUID
+        memex.createLink(link: link, completion: { (newLink, error) in
+          XCTAssertNil(error, "request failed")
+          XCTAssertNotNil(newSpace?.MUID, "missing ID")
+          XCTAssertNotNil(newSpace?.createdAt, "missing created at")
+          XCTAssertNotNil(newSpace?.updatedAt, "missing updated at")
+          XCTAssertTrue(newSpace?.MUID == space.MUID, "wrong MUID")
+          XCTAssertTrue(newSpace?.state == .visible, "wrong visibility state")
+          XCTAssertNotNil(newSpace?.ownerID, "missing owner")
+          XCTAssertTrue(newSpace?.ownerID == myself?.ID, "wrong owner")
+          XCTAssertTrue(newLink?.originSpaceMUID == newSpace?.MUID, "wrong origin space")
+          XCTAssertTrue(newLink?.targetSpaceMUID == newSpace?.MUID, "wrong target space")
+          expectation1.fulfill()
+        })
+        
+      })
+    }
+    waitForExpectations(timeout: Constants.timeout, handler: nil)
+  }
+  
+  func testGetSpaceLinks() {
+    let expectation1 = expectation(description: "default")
+    self.prepareSDK(authorize: true) { (memex, myself) in
+      let space = Space()
+      space.MUID = UUID().uuidString
+      memex.createSpace(space: space, process: .no, autodump: false, completion: { (newSpace, error) in
+        XCTAssertNil(error, "request failed")
+        
+        let link = Link()
+        link.MUID = UUID().uuidString
+        link.originSpaceMUID = newSpace?.MUID
+        link.targetSpaceMUID = newSpace?.MUID
+        memex.createLink(link: link, completion: { (newLink, error) in
+          XCTAssertNil(error, "request failed")
+          memex.getSpaceLinks(muid: space.MUID!, completion: { (links, error) in
+            XCTAssertNil(error, "request failed")
+            XCTAssertTrue(links?.count == 1, "wrong number of links")
+            let getLink = links!.first!
+            XCTAssertTrue(getLink.MUID == link.MUID, "wrong number of links")
+            expectation1.fulfill()
+          })
+        })
+        
+      })
+    }
+    waitForExpectations(timeout: Constants.timeout, handler: nil)
+  }
+  
+  func testPushPull() {
+    let expectation1 = expectation(description: "default")
+    self.prepareSDK(authorize: true) { (memex, myself) in
+      let space = Space()
+      space.MUID = UUID().uuidString
+      memex.createSpace(space: space, process: .no, autodump: false, completion: { (newSpace, error) in
+        XCTAssertNil(error, "request failed")
+        
+        let link = Link()
+        link.MUID = UUID().uuidString
+        link.originSpaceMUID = newSpace?.MUID
+        link.targetSpaceMUID = newSpace?.MUID
+      
+        memex.pushLinks(items: [link], completion: { (oldModelVersion, newModelVersion, error) in
+          XCTAssertNil(error, "request failed")
+          XCTAssertTrue(oldModelVersion == 1, "wrong old model version")
+          XCTAssertTrue(newModelVersion == 2, "wrong new model version")
+          memex.pullLinks(lastModelVersion: newModelVersion, offset: 0, completion: { (returnedLinks, modelVersion, total, more, nextOffset, error) in
+            XCTAssertNil(error, "request failed")
+            XCTAssertTrue(returnedLinks!.count == 0, "wrong number of links")
+            XCTAssertTrue(total == 0, "wrong number of total")
+            XCTAssertTrue(nextOffset == nil, "wrong next offset")
+            XCTAssertTrue(more == false, "wrong number of total")
+            XCTAssertTrue(modelVersion == newModelVersion, "wrong old model version")
+            
+            memex.pullLinks(lastModelVersion: nil, offset: 0, completion: { (returnedLinks, modelVersion, total, more, nextOffset, error) in
+              XCTAssertNil(error, "request failed")
+              XCTAssertTrue(returnedLinks!.count >= 1, "wrong number of spaces")
+              XCTAssertTrue(returnedLinks!.filter({ $0.MUID! == link.MUID!}).count >= 1, "new link not found")
+              XCTAssertTrue(total! >= 1, "wrong number of total")
+              XCTAssertTrue(nextOffset == nil, "wrong next offset")
+              XCTAssertTrue(more == false, "wrong number of total")
+              XCTAssertTrue(modelVersion == newModelVersion, "wrong old model version")
+              expectation1.fulfill()
+            })
+            
+          })
+          
+        })
+      })
+      
+    }
+    waitForExpectations(timeout: Constants.timeout, handler: nil)
+  }
+
+  
+}
--- Sources/LinksHandler.swift
+++ Sources/LinksHandler.swift
@@ -5,6 +5,66 @@ import ObjectMapper
 public extension Spaces {
   
   /**
+   New link creation.
+   
+   - parameter link: New link object
+   - parameter completion: Completion block
+   - parameter link: Link object with valid dataUploadURL
+   - parameter error: Error if something wrong happens
+   
+   */
+  public func createLink(link: Link,
+                         completion: @escaping (_ media: Link?, _ error: Swift.Error?)->()) {
+    POST("links",
+         parameters: [
+          "link": Mapper<Link>().toJSON(link)
+    ]) { [weak self] response in
+      completion(self?.entityFromDictionary(dictionary: response.contentDictionary?["link"]),
+                 response.error)
+    }
+  }
+  
+  /**
+   Method for getting space links
+   
+   - parameter muid: MUID of space or 'origin'
+   - parameter completion: Completion block.
+   - parameter links: Fetched space links.
+   - parameter error: Error message if something wrong happens.
+   
+   */
+  public func getSpaceLinks(muid: String,
+                            completion: @escaping (_ space: [Link]?, _ error: Swift.Error?)->()) {
+    GET("spaces/\(muid)/links") { [weak self] response in
+      let links: [Link]? = self?.entitiesFromArray(array: response.contentDictionary?["links"])
+      completion(links, response.error)
+    }
+  }
+  
+  /**
+   This method allows you to sync multiple links
+   
+   - parameter items: Set of new or changed links
+   - parameter removeToken: Entities can have assigned remove token and it can be used to easily remove them before they are replaced by another ones
+   - parameter completion: Completion block
+   
+   */
+  public func pushLinks(items: [Link],
+                        removeToken: String? = nil,
+                        completion: @escaping PushOutputs) {
+    var parameters = [String: Any]()
+    parameters["links"] = items.toJSON()
+    if let value = removeToken {
+      parameters["remove_token"] = value
+    }
+    POST("links/multiple",parameters: parameters) { response in
+      let oldModelVersion = response.contentDictionary?["old_model_version"] as? Int
+      let modelVersion = response.contentDictionary?["model_version"] as? Int
+      completion(oldModelVersion, modelVersion, response.error)
+    }
+  }
+  
+  /**
    Method for fetching all accessible links.
    
    - parameter lastModelVersion: Last user model version that was fetched (allows diff downloads)
@@ -37,67 +97,6 @@ public extension Spaces {
           completion(items, modelVersion, totalItems, hasMore, nextOffset, response.error)
     }
   }
-  
-  /**
-   New link creation.
-   
-   - parameter link: New link object
-   - parameter completion: Completion block
-   - parameter link: Link object with valid dataUploadURL
-   - parameter error: Error if something wrong happens
-   
-   */
-  public func createLink(link: Link,
-                         completion: @escaping (_ media: Link?, _ error: Swift.Error?)->()) {
-    POST("links",
-         parameters: [
-          "link": Mapper<Link>().toJSON(link)
-    ]) { [weak self] response in
-      completion(self?.entityFromDictionary(dictionary: response.contentDictionary?["link"]),
-                 response.error)
-    }
-  }
-  
-  /**
-   This method allows you to sync multiple links
-   
-   - parameter items: Set of new or changed links
-   - parameter removeToken: Entities can have assigned remove token and it can be used to easily remove them before they are replaced by another ones
-   - parameter completion: Completion block
-   
-   */
-  public func pushLinks(items: [Link],
-                        removeToken: String? = nil,
-                        completion: @escaping PushOutputs) {
-    var parameters = [String: Any]()
-    parameters["links"] = items.toJSON()
-    if let value = removeToken {
-      parameters["remove_token"] = value
-    }
-    POST("links/multiple",parameters: parameters) { response in
-      let oldModelVersion = response.contentDictionary?["old_model_version"] as? Int
-      let modelVersion = response.contentDictionary?["model_version"] as? Int
-      completion(oldModelVersion, modelVersion, response.error)
-    }
-  }
-  
-  /**
-   Method for getting space links
-   
-   - parameter muid: MUID of space or 'origin'
-   - parameter completion: Completion block.
-   - parameter links: Fetched space links.
-   - parameter error: Error message if something wrong happens.
-   
-   */
-  public func getSpaceLinks(muid: String,
-                       completion: @escaping (_ space: [Link]?, _ error: Swift.Error?)->()) {
-    GET("spaces/\(muid)/links") { [weak self] response in
-      let links: [Link]? = self?.entitiesFromArray(array: response.contentDictionary?["links"])
-      completion(links, response.error)
-    }
-  }
-  
   
 }
 
